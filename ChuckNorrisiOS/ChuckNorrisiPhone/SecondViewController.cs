581158d054d02cbeb3182b9f4a3805aafa7fd470
richharms <rh-github@darkrealms.com>
2015-04-18 22:21:15 -0500

    Implemented graph

--- ChuckNorrisiOS/ChuckNorrisiPhone/SecondViewController.cs
+++ ChuckNorrisiOS/ChuckNorrisiPhone/SecondViewController.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using Foundation;
 using UIKit;
 using ChuckNorris;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using OxyPlot.Xamarin.iOS;
+using CoreGraphics;
 
 namespace ChuckNorrisiPhone
 {
@@ -27,11 +31,49 @@ namespace ChuckNorrisiPhone
 
         #region View lifecycle
 
+        private PlotModel CreatePlotModel()
+        {
+            ChuckNorrisActions chuckNorrisActions = new ChuckNorrisActions();
+
+            var plotModel = new PlotModel { Title = chuckNorrisActions.Title };
+
+            //plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+            //plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0 });
+
+            plotModel.Series.Add(new LineSeries
+            {
+                ItemsSource = chuckNorrisActions.Kicks,
+                Title = "Kicks"
+            });
+
+            plotModel.Series.Add(new LineSeries
+            {
+                ItemsSource = chuckNorrisActions.Punches,
+                Title = "Punches"
+            });
+
+            return plotModel;
+        }
+
+        PlotView plotView;
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
             // Perform any additional setup after loading the view, typically from a nib.
+            //PlotView plotView = new PlotView();
+            var plotModel = new PlotModel { Title = "OxyPlot Demo" };
+
+            CGRect viewFrame = this.View.Frame;
+            CGRect graphFrame = new CGRect(viewFrame.Left, viewFrame.Top + 10, viewFrame.Right - 10, viewFrame.Bottom - 60);
+
+            plotView = new PlotView
+            {
+                Model = CreatePlotModel(),
+                Frame = graphFrame
+            };
+            this.View.AddSubview(plotView);
         }
 
         public override void ViewWillAppear(bool animated)
@@ -52,6 +94,16 @@ namespace ChuckNorrisiPhone
         public override void ViewDidDisappear(bool animated)
         {
             base.ViewDidDisappear(animated);
+        }
+
+        public override void DidRotate(UIInterfaceOrientation fromInterfaceOrientation)
+        {
+            base.DidRotate(fromInterfaceOrientation);
+
+            CGRect viewFrame = this.View.Frame;
+            CGRect graphFrame = new CGRect(viewFrame.Left, viewFrame.Top + 10, viewFrame.Right - 10, viewFrame.Bottom - 60);
+            this.plotView.Frame = graphFrame;
+            this.plotView.InvalidatePlot(false);
         }
 
         #endregion
